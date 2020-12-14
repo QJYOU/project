c02e4ba2b5f584fa2641a22cc3eb9138106741af
E84061155 <e0425820@gmail.com>
2020-12-14 11:50:59 +0800

    database

--- graph/graph/Form1.Designer.cs
+++ graph/graph/Form1.Designer.cs
@@ -35,35 +35,57 @@
             System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
-            this.linechart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.calories_Chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.piechart = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.datePickbutton = new System.Windows.Forms.Button();
             this.BegindatePicker = new System.Windows.Forms.DateTimePicker();
             this.EnddatePicker = new System.Windows.Forms.DateTimePicker();
             this.label1 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.linechart)).BeginInit();
+            this.label2 = new System.Windows.Forms.Label();
+            this.fat_chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.sugar_chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.protein_chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.carbonhydrate_chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.calories_Chart)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.piechart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fat_chart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sugar_chart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.protein_chart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carbonhydrate_chart)).BeginInit();
             this.SuspendLayout();
             // 
-            // linechart
+            // calories_Chart
             // 
-            this.linechart.BackColor = System.Drawing.Color.Transparent;
+            this.calories_Chart.BackColor = System.Drawing.Color.Transparent;
             chartArea1.Name = "ChartArea1";
-            this.linechart.ChartAreas.Add(chartArea1);
+            this.calories_Chart.ChartAreas.Add(chartArea1);
             legend1.Enabled = false;
             legend1.Name = "Legend1";
-            this.linechart.Legends.Add(legend1);
-            this.linechart.Location = new System.Drawing.Point(521, 61);
-            this.linechart.Name = "linechart";
+            this.calories_Chart.Legends.Add(legend1);
+            this.calories_Chart.Location = new System.Drawing.Point(56, 12);
+            this.calories_Chart.Name = "calories_Chart";
             series1.ChartArea = "ChartArea1";
             series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
             series1.IsValueShownAsLabel = true;
             series1.Legend = "Legend1";
             series1.Name = "Series1";
-            this.linechart.Series.Add(series1);
-            this.linechart.Size = new System.Drawing.Size(402, 233);
-            this.linechart.TabIndex = 1;
-            this.linechart.Text = "chart2";
+            this.calories_Chart.Series.Add(series1);
+            this.calories_Chart.Size = new System.Drawing.Size(944, 510);
+            this.calories_Chart.TabIndex = 1;
+            this.calories_Chart.Text = "Calories_Chart";
             // 
             // piechart
             // 
@@ -92,17 +114,18 @@
             // datePickbutton
             // 
             this.datePickbutton.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.datePickbutton.Location = new System.Drawing.Point(621, 370);
+            this.datePickbutton.Location = new System.Drawing.Point(696, 551);
             this.datePickbutton.Name = "datePickbutton";
             this.datePickbutton.Size = new System.Drawing.Size(97, 32);
             this.datePickbutton.TabIndex = 2;
-            this.datePickbutton.Text = "選擇日期";
+            this.datePickbutton.Text = "Select";
             this.datePickbutton.UseVisualStyleBackColor = true;
             this.datePickbutton.Click += new System.EventHandler(this.datePickbutton_Click);
             // 
             // BegindatePicker
             // 
-            this.BegindatePicker.Location = new System.Drawing.Point(259, 373);
+            this.BegindatePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.BegindatePicker.Location = new System.Drawing.Point(228, 551);
             this.BegindatePicker.Name = "BegindatePicker";
             this.BegindatePicker.Size = new System.Drawing.Size(145, 29);
             this.BegindatePicker.TabIndex = 3;
@@ -110,7 +133,8 @@
             // 
             // EnddatePicker
             // 
-            this.EnddatePicker.Location = new System.Drawing.Point(432, 373);
+            this.EnddatePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.EnddatePicker.Location = new System.Drawing.Point(517, 551);
             this.EnddatePicker.Name = "EnddatePicker";
             this.EnddatePicker.Size = new System.Drawing.Size(145, 29);
             this.EnddatePicker.TabIndex = 4;
@@ -119,40 +143,158 @@
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("微軟正黑體", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label1.Location = new System.Drawing.Point(102, 370);
+            this.label1.Location = new System.Drawing.Point(78, 551);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(151, 32);
+            this.label1.Size = new System.Drawing.Size(120, 32);
             this.label1.TabIndex = 5;
-            this.label1.Text = "日期範圍";
+            this.label1.Text = "Date：";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(434, 544);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 32);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "to";
+            // 
+            // fat_chart
+            // 
+            this.fat_chart.BackColor = System.Drawing.Color.Transparent;
+            chartArea3.Name = "ChartArea1";
+            this.fat_chart.ChartAreas.Add(chartArea3);
+            legend3.Enabled = false;
+            legend3.Name = "Legend1";
+            this.fat_chart.Legends.Add(legend3);
+            this.fat_chart.Location = new System.Drawing.Point(381, 40);
+            this.fat_chart.Name = "fat_chart";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.IsValueShownAsLabel = true;
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.fat_chart.Series.Add(series3);
+            this.fat_chart.Size = new System.Drawing.Size(306, 177);
+            this.fat_chart.TabIndex = 7;
+            this.fat_chart.Text = "fat_chart";
+            this.fat_chart.Visible = false;
+            // 
+            // sugar_chart
+            // 
+            this.sugar_chart.BackColor = System.Drawing.Color.Transparent;
+            chartArea4.Name = "ChartArea1";
+            this.sugar_chart.ChartAreas.Add(chartArea4);
+            legend4.Enabled = false;
+            legend4.Name = "Legend1";
+            this.sugar_chart.Legends.Add(legend4);
+            this.sugar_chart.Location = new System.Drawing.Point(652, 162);
+            this.sugar_chart.Name = "sugar_chart";
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.IsValueShownAsLabel = true;
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.sugar_chart.Series.Add(series4);
+            this.sugar_chart.Size = new System.Drawing.Size(306, 177);
+            this.sugar_chart.TabIndex = 8;
+            this.sugar_chart.Text = "sugar_chart";
+            this.sugar_chart.Visible = false;
+            // 
+            // protein_chart
+            // 
+            this.protein_chart.BackColor = System.Drawing.Color.Transparent;
+            chartArea5.Name = "ChartArea1";
+            this.protein_chart.ChartAreas.Add(chartArea5);
+            legend5.Enabled = false;
+            legend5.Name = "Legend1";
+            this.protein_chart.Legends.Add(legend5);
+            this.protein_chart.Location = new System.Drawing.Point(211, 162);
+            this.protein_chart.Name = "protein_chart";
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series5.IsValueShownAsLabel = true;
+            series5.Legend = "Legend1";
+            series5.Name = "Series1";
+            this.protein_chart.Series.Add(series5);
+            this.protein_chart.Size = new System.Drawing.Size(306, 177);
+            this.protein_chart.TabIndex = 9;
+            this.protein_chart.Text = "protein_chart";
+            this.protein_chart.Visible = false;
+            // 
+            // carbonhydrate_chart
+            // 
+            this.carbonhydrate_chart.BackColor = System.Drawing.Color.Transparent;
+            chartArea6.Name = "ChartArea1";
+            this.carbonhydrate_chart.ChartAreas.Add(chartArea6);
+            legend6.Enabled = false;
+            legend6.Name = "Legend1";
+            this.carbonhydrate_chart.Legends.Add(legend6);
+            this.carbonhydrate_chart.Location = new System.Drawing.Point(732, 40);
+            this.carbonhydrate_chart.Name = "carbonhydrate_chart";
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series6.IsValueShownAsLabel = true;
+            series6.Legend = "Legend1";
+            series6.Name = "Series1";
+            this.carbonhydrate_chart.Series.Add(series6);
+            this.carbonhydrate_chart.Size = new System.Drawing.Size(306, 177);
+            this.carbonhydrate_chart.TabIndex = 10;
+            this.carbonhydrate_chart.Text = "carbonhydrate_chart";
+            this.carbonhydrate_chart.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(860, 459);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(278, 146);
+            this.textBox1.TabIndex = 11;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Snow;
-            this.ClientSize = new System.Drawing.Size(1046, 560);
+            this.ClientSize = new System.Drawing.Size(1195, 617);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.carbonhydrate_chart);
+            this.Controls.Add(this.protein_chart);
+            this.Controls.Add(this.sugar_chart);
+            this.Controls.Add(this.fat_chart);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.EnddatePicker);
             this.Controls.Add(this.BegindatePicker);
             this.Controls.Add(this.datePickbutton);
-            this.Controls.Add(this.linechart);
+            this.Controls.Add(this.calories_Chart);
             this.Controls.Add(this.piechart);
             this.Name = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.linechart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.calories_Chart)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.piechart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fat_chart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sugar_chart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.protein_chart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carbonhydrate_chart)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataVisualization.Charting.Chart linechart;
+        private System.Windows.Forms.DataVisualization.Charting.Chart calories_Chart;
         private System.Windows.Forms.DataVisualization.Charting.Chart piechart;
         private System.Windows.Forms.Button datePickbutton;
         private System.Windows.Forms.DateTimePicker BegindatePicker;
         private System.Windows.Forms.DateTimePicker EnddatePicker;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart sugar_chart;
+        private System.Windows.Forms.DataVisualization.Charting.Chart protein_chart;
+        private System.Windows.Forms.DataVisualization.Charting.Chart carbonhydrate_chart;
+        private System.Windows.Forms.DataVisualization.Charting.Chart fat_chart;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- graph/graph/Form1.cs
+++ graph/graph/Form1.cs
@@ -13,13 +13,23 @@ namespace graph
 {
     public partial class Form1 : Form
     {
-        SqlConnection conn;
-        SqlCommand cmd;
-        string path = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\ee258\Desktop\c#\groupProject\graph\graph\Database.mdf;Integrated Security=True";
+        string account = "qq";
         string BeginDate = "";
         string EndDate = "";
-        string[] xvalue = { "澱粉", "蛋白質", "脂肪", "1", "2" };
-        int[] yvalue = { 5, 18, 45, 17, 2 };
+        data d = new data();
+
+        int begin_date = 20201213;
+        int end_date = 20201218;
+        List<string>[] list = new List<string>[6];
+
+
+        List<string> calories_list = new List<string>();
+        List<string> sugar_list = new List<string>();
+        List<string> fat_list = new List<string>();
+        List<string> carbonhydrate_list = new List<string>();
+        List<string> protein_list = new List<string>();
+        List<string> date_list = new List<string>();
+        int days = 0;
 
         public Form1()
         {
@@ -27,15 +37,23 @@ namespace graph
             BegindatePicker.Visible = true;
             EnddatePicker.Visible = true;
             piechart.Visible = false;
-            linechart.Visible = false;
-            conn = new SqlConnection();
-            conn.ConnectionString = path;
-            conn.Open();
-            string sqlstr = "CREATE TABLE [dbo].[data] (col0 numeric identity (1,1) primary key,)";
-            cmd = new SqlCommand(sqlstr,conn);
-            cmd.ExecuteNonQuery();
+            calories_Chart.Visible = false;
+            
+            for(int i=0;i<6;++i)
+            {
+                list[i] = new List<string>();
+            }
         }
 
+        public Form1(string username)
+        {
+            InitializeComponent();
+            BegindatePicker.Visible = true;
+            EnddatePicker.Visible = true;
+            piechart.Visible = false;
+            calories_Chart.Visible = false;
+            account = username;
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
 
@@ -43,30 +61,65 @@ namespace graph
 
         private void BegindatePicker_ValueChanged(object sender, EventArgs e)
         {
-            BeginDate = "";
-            BeginDate += BegindatePicker.Value.Year.ToString();
-            BeginDate += BegindatePicker.Value.Month.ToString();
-            BeginDate += BegindatePicker.Value.Day.ToString();
+            BeginDate = BegindatePicker.Value.Year.ToString() + BegindatePicker.Value.Month.ToString()+ BegindatePicker.Value.Day.ToString();
+            begin_date = int.Parse(BeginDate);
+           // BeginDate += BegindatePicker.Value.Month.ToString();
+            //BeginDate += BegindatePicker.Value.Day.ToString();
+
         }
 
         private void datePickbutton_Click(object sender, EventArgs e)
         {
+            Read(begin_date,end_date);
+
+            //foreach (string item in list[0])
+            //    textBox1.Text += item;
+            textBox1.Text+=
             piechart.Visible = true;
-            linechart.Visible = true;
+            calories_Chart.Visible = true;
             piechart.Series[0]["PieLabelStyle"] = "outside";
-            piechart.Series["Series1"].Points.DataBindXY(xvalue, yvalue);
-            //chart1.Series[0].LegendText = "#VALX:[#PERCENT{P1}]";
-            // chart1.Series[0].Label = "#VALX\n#PERCENT{P1}";
+            
             piechart.Series[0].IsValueShownAsLabel = true;
-            linechart.Series[0].Points.DataBindXY(xvalue, yvalue);
+            calories_Chart.Series[0].Points.DataBindXY(list[1], list[1]);
+
         }
 
         private void EnddatePicker_ValueChanged(object sender, EventArgs e)
         {
-            EndDate = "";
-            EndDate += EnddatePicker.Value.Year.ToString();
-            EndDate += EnddatePicker.Value.Month.ToString();
-            EndDate += EnddatePicker.Value.Month.ToString();
+            EndDate = EnddatePicker.Value.Year.ToString() + EnddatePicker.Value.Month.ToString()+EnddatePicker.Value.Day.ToString();
+            //EndDate += EnddatePicker.Value.Month.ToString();
+            //EndDate += EnddatePicker.Value.Month.ToString();
+            end_date = int.Parse(EndDate);
+                      
+        }
+
+        private void Read(int begin,int end)
+        {
+            d.select(account+"2");
+            d.between(begin, end);
+            d.work();
+            d.connect();
+            DataTable dt = d.rd.GetSchemaTable();
+            int i=0;
+            int j = 0;
+            foreach (DataRow row in dt.Rows)
+            {
+                j = 0;
+                foreach (DataColumn column in dt.Columns)
+                {
+                    if (j == 0) j++;
+                    else
+                    {
+                        list[i].Add(row[column].ToString());
+                        textBox1.Text += row[column].ToString() + "\r\n";
+                    }
+                    
+
+                }
+                if (i == 0) ++i;
+
+            }
+            d.disconnect();
         }
     }
 }
